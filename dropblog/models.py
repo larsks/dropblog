53a8e672e7511210804b07414d5f81aaf31ee22a
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2012-11-13 13:42:01 -0500

    added Blog and Post models

--- dropblog/models.py
+++ dropblog/models.py
@@ -3,9 +3,12 @@
 import os
 import sys
 
-from sqlalchemy import Column, Integer, String, Text, create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, Text, Boolean
+from sqlalchemy import ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.interfaces import PoolListener
 
 Base = declarative_base()
 Session = sessionmaker()
@@ -19,14 +22,51 @@ class Setting (Base):
 class Identity (Base):
     __tablename__ = 'identities'
 
-    uid = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True)
     display_name = Column(String)
     email = Column(String)
+
     dropbox_key = Column(String)
     dropbox_secret = Column(String)
+    dropbox_cursor = Column(String)
+
+    blogs = relationship("Blog", backref="owner")
+
+class Blog (Base):
+    __tablename__ = 'blogs'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    title = Column(String)
+
+    posts = relationship("Post", backref="blog")
+
+    owner_id = Column(Integer, ForeignKey("identities.id"))
+
+class Post (Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    path = Column(String)
+    slug = Column(String)
+    title = Column(String)
+    date = Column(String)
+    published = Column(Boolean)
+
+    blog_id = Column(Integer, ForeignKey("blogs.id"))
+
+class ForeignKeysListener(PoolListener):
+    def connect(self, dbapi_con, con_record):
+        db_cursor = dbapi_con.execute('pragma foreign_keys=ON')
 
 def init(dburi, echo=False):
-    engine = create_engine(dburi, echo=echo)
+    listeners = []
+
+    # Enable foreign key enforcement under sqlite.
+    if dburi.startswith('sqlite:'):
+        listeners.append(ForeignKeysListener())
+
+    engine = create_engine(dburi, echo=echo, listeners=listeners)
     Base.metadata.create_all(engine)
     Session.configure(bind=engine)
 
