103b88f82d6a9db0383f607fa066d52ea5273a80
Lars Kellogg-Stedman <lars@seas.harvard.edu>
2012-11-13 09:05:05 -0500

    packaging prep

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a simple Dropbox-backed blogging platform.
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'dropblog',
+    description = 'A Dropbox backed blogging platform',
+    long_description = open('README.md').read(),
+    author = 'Lars Kellogg-Stedman',
+    author_email = 'lars@oddbit.com',
+    version = "1.00",
+    packages = find_packages(),
+    install_requires = [
+        'bottle',
+        'beaker',
+        'oath',
+        'dropbox',
+        'markdown',
+        ],
+    entry_points = {
+#        'console_scripts': [
+#            'mimecraft = mimecraft.main:main',
+#            ],
+        },
+)
+
