1fc86255e662c7f55614d72d948c98adfb0ed307
Lars Kellogg-Stedman <lars@oddbit.com>
2012-11-13 23:02:01 -0500

    hey, we can parse delta responses

--- dropblog/update.py
+++ dropblog/update.py
@@ -3,11 +3,19 @@
 import os
 import sys
 import argparse
+import mailbox
+import re
+import time
 
 import models
+from models import *
 import dropbox
 
 import utils
+
+transtable = ''.join(chr(x) if (chr(x).isalpha()) else '-' for x in range(0,256))
+deltable = ''.join(chr(x) for x in range(0,255) if not chr(x).isalpha() and
+        not chr(x) in [' ', '-'])
 
 def parse_args():
     p = argparse.ArgumentParser()
@@ -16,14 +24,17 @@ def parse_args():
     p.add_argument('--debug', action='store_true')
     return p.parse_args()
 
+def slugify(title):
+    return title.translate(transtable, deltable).lower()[:40]
+
 def main():
     opts = parse_args()
     models.init(opts.dburi, echo=opts.debug)
-    db = models.Session()
+    db = Session()
 
     dbx = dropbox.client.DropboxClient(utils.dropbox_session(db))
 
-    for owner in db.query(models.Identity):
+    for owner in db.query(Identity):
         blogs = {}
         cursor = owner.dropbox_cursor
         print 'Found cursor:', cursor
@@ -37,8 +48,43 @@ def main():
         
         delta = dbx.delta(cursor)
 
+        if delta['reset']:
+            print 'RESET'
+            for post in db.query(Post).join(Post.blog).filter(Blog.owner_id ==
+                    owner.id):
+                db.delete(post)
+            db.commit()
+
         for entry in delta['entries']:
-            print entry[0]
+            if entry[1] and entry[1]['is_dir']:
+                continue
+
+            mo = re.match('/sites/(?P<blog>[^/]*)/posts/(?P<post>[^/]*\.md)', entry[0])
+            if not mo or mo.group('blog') not in blogs:
+                continue
+
+            print '->', entry[0]
+
+            if entry[1] is None:
+                db.query(Post).filter(Post.path == entry[0]).delete()
+            else:
+                doc = mailbox.Message(dbx.get_file(entry[0]))
+
+                p = db.query(Post).get(entry[0])
+
+                if not p:
+                    p = Post(id=entry[0],
+                        date=time.strftime('%Y-%m-%d',
+                            time.localtime()))
+                    db.add(p)
+
+                blogs[mo.group('blog')].posts.append(p)
+
+                p.title = doc.get('title',
+                        os.path.splitext(os.path.basename(entry[0]))[0]).encode('utf-8')
+                p.slug = slugify(p.title)
+                p.published = doc.get('published', 'True') == 'True'
+                p.date = doc.get('date', p.date)
 
         owner.dropbox_cursor = delta['cursor']
         db.commit()
